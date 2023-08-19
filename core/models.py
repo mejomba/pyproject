fe3fca124e081585303ac7baf34a0656ae334214
mejomba <moojaminzadeh@gmail.com>
2023-08-19 21:26:48 +0330

    add UserProfile model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
 
 class Image(models.Model):
     path = models.ImageField(upload_to='media')
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
