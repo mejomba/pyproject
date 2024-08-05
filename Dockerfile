fca67194f4f4f6a3ecab7e6cad53b64369ba9cba
mejomba <moojaminzadeh@gmail.com>
2024-08-05 22:04:13 +0330

    f

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,42 @@ RUN python -m venv /venv && \
     apk add --update --virtual .tmp-deps \
         build-base postgresql-dev musl-dev linux-headers
 
+RUN apt-get install --yes --quiet --no-install-recommends \
+    gettext \
+    fonts-liberation \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libatspi2.0-0 \
+    libcairo2 \
+    libcups2 \
+    libdbus-1-3 \
+    libdrm2 \
+    libgbm1 \
+    libgdk-pixbuf2.0-0 \
+    libglib2.0-0 \
+    libgtk-3-0 \
+    libnspr4 \
+    libnss3 \
+    libpango-1.0-0 \
+    libx11-6 \
+    libxcb1 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxkbcommon0 \
+    libxrandr2 \
+    libxshmfence1 \
+    wget \
+    xdg-utils \
+    netcat \
+    xvfb \
+ && rm -rf /var/lib/apt/lists/*
+
+# Install Chrome
+RUN dpkg -i ./bin/google-chrome.deb
+
 RUN /venv/bin/pip install -r /requirements.txt && \
     apk del .tmp-deps && \
     /venv/bin/python manage.py collectstatic --noinput && \
--- finance/scraper.py
+++ finance/scraper.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -8,10 +7,44 @@ from webdriver_manager.chrome import ChromeDriverManager
 # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 # driver = webdriver.Firefox()#Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-from chromedriver_py import binary_path # This will get you the path variable
 
-svc = webdriver.ChromeService(executable_path=binary_path)
-driver = webdriver.Chrome(service=svc)
+import os
+from django.conf import settings
+
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
+WINDOW_SIZE = "1920x1080"
+
+
+class Browser(webdriver.Chrome):
+    timeout = 15
+
+    def __init__(self):
+        # Adds the executable folder to PATH so that chromedriver is available
+        path_bin = str(settings.BASE_DIR / 'bin')
+        if path_bin not in os.environ["PATH"]:
+            os.environ["PATH"] += os.pathsep + path_bin
+
+        # Customise Chrome options
+        chrome_options = webdriver.ChromeOptions()
+
+        # Allows you to launch Chrome without a window
+        chrome_options.add_argument('--headless')
+
+        # Google recommends this option
+        chrome_options.add_argument("--no-sandbox")
+
+        # Fixing issues with Docker memory
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+        # Allows you to customise the USER_AGENT
+        chrome_options.add_argument(f"user-agent={USER_AGENT}")
+
+        # Allows you to change the size of the window
+        chrome_options.add_argument(f"window-size={WINDOW_SIZE}")
+
+        super().__init__(chrome_options=chrome_options)
+
+driver = Browser()
 
 
 def scrap_codal(symbol):
