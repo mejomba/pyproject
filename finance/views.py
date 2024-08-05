376b9987513a1d74c3aca8cf6f9c3a2a91ddc35f
mejomba <moojaminzadeh@gmail.com>
2024-08-05 20:22:01 +0330

    777 permision

--- finance/views.py
+++ finance/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import View
 
-from .scraper import scrap_codal
+# from .scraper import scrap_codal
 
 
-class CodalNewReport(View):
-    template_name = 'finance/index.html'
-
-    def get(self, request, *args, **kwargs):
-        content = scrap_codal('وکغدیر')
-        context = {'links': content}
-        return render(request, self.template_name, context)
+# class CodalNewReport(View):
+#     template_name = 'finance/index.html'
+#
+#     def get(self, request, *args, **kwargs):
+#         content = scrap_codal('وکغدیر')
+#         context = {'links': content}
+#         return render(request, self.template_name, context)
