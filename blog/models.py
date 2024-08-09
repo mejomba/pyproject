6893fc525ede5d9c772bd183727aaa53d9bc5d09
mejomba <moojaminzadeh@gmail.com>
2024-08-09 15:57:08 +0330

    update post mode

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,8 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class Post(BaseModel):
-    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    abstract = models.CharField(max_length=255)
-    content = RichTextUploadingField()
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name='نویسنده')
+    abstract = models.CharField(max_length=255, verbose_name='چکیده')
+    content = RichTextUploadingField(verbose_name='محتوا')
+    # thumbnail =  generic image
+    title = models.CharField(max_length=255, verbose_name='عنوان')
